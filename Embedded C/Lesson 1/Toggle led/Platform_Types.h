5c0a41700361a14e3dab2e58a8ac7b20e5e87bbe
Ahmed Mohammed Hussanein <engineer.ahmed1812@gmail.com>
2022-08-13 14:08:54 +0200

    Updating

--- Embedded C/Lesson 1/Toggle led/Platform_Types.h
+++ Embedded C/Lesson 1/Toggle led/Platform_Types.h
@@ -1,10 +1,20 @@
-#ifndef __PLATFORM_TYPES_H__
-#define __PLATFORM_TYPES_H__
+#ifndef PLATFORM_TYPES_H_
+#define PLATFORM_TYPES_H_
+
+#define CPU_TYPE_8        8
+#define CPU_TYPE_16      16
+#define CPU_TYPE_32      32
+
+#define MSB_FIRST        0
+#define LSB_FIRST        1
+
+#define HIGH_BYTE_FIRST  0
+#define LOW_BYTE_FIRST   1
 
 /* Little endian, 32 bit processor */
-#define CPU_TYPE			CPU_TYPE_32
-#define CPU_BIT_ORDER   	LSB_FIRST	
-#define CPU_BYTE_ORDER  	LOW_BYTE_FIRST 
+#define CPU_TYPE			    CPU_TYPE_8
+#define CPU_BIT_ORDER   	LSB_FIRST
+#define CPU_BYTE_ORDER  	LOW_BYTE_FIRST
 
 typedef unsigned char 		boolean;
 
@@ -21,12 +31,12 @@ typedef signed char			sint8;
 typedef unsigned char		uint8;
 
 typedef signed short		sint16;
-typedef unsigned short		uint16;
+typedef unsigned short  uint16;
 
 typedef signed int			sint32;
 typedef unsigned int		uint32;
 
-typedef float				float32;
-typedef double				float64;
+typedef float				    float32;
+typedef double				  float64;
 
 #endif
